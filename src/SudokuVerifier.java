8d051c2acf00733e902b0ac5112c132e6ca3add4
keios <keios@10.20.192.144>
2017-09-27 17:05:25 +0300

    initial commit

--- src/SudokuVerifier.java
+++ src/SudokuVerifier.java
@@ -1,10 +1,36 @@
 
 public class SudokuVerifier {
-	
+	int[][] sudokuMatrix = new int[9][9];
 	public int verify(String candidateSolution) {
 		// returns 0 if the candidate solution is correct
-		return 0;
+		if (!verifyString(candidateSolution)) {
+			return -1;
+		}
 		
-		//check
+		return 0;
 	}
+	
+	public boolean verifyString(String str) {
+		if (str.length() != 81) return false;
+		
+		for (char c : str.toCharArray()) {
+			if (!Character.isDigit(c) || c == '0') return false;
+		}
+		return true;
+	}
+	
+	public void buildMatrix(String sudoku) {
+		for (int i = 0; i < 9; i++) {
+			for (int j = 0; j < 9; j++) {
+				this.sudokuMatrix[j][i] = sudoku.charAt(0);
+				sudoku = sudoku.substring(1);
+			}
+		}
+	}
+	
+	public int[][] getSudokuMatrix() {
+		return this.sudokuMatrix;
+	}
+	
+	
 }
--- tests/SudokuVerifierTest.java
+++ tests/SudokuVerifierTest.java
@@ -8,8 +8,64 @@ public class SudokuVerifierTest {
 // An incorrect Sudoku string: 123456789912345678891234567789123456678912345567891234456789123345678912234567891
 
 	@Test
-	public void testVerify() {
-		fail("Not yet implemented");
+	public void testVerifyStringShort() {
+		SudokuVerifier verifier = new SudokuVerifier();
+		
+		boolean result = verifier.verifyString("1");
+		
+		assertEquals("String should've been wrong", false, result);
 	}
+	
+	@Test
+	public void testVerifyStringNotDigit() {
+		SudokuVerifier verifier = new SudokuVerifier();
+		
+		boolean result = verifier.verifyString("asd");
+		
+		assertEquals("String should've been wrong", false, result);
+	}
+	
+	@Test
+	public void testVerifyStringTooLong() {
+		SudokuVerifier verifier = new SudokuVerifier();
+		
+		boolean result = verifier.verifyString("4173698256321589479587243168254371697915864323469127582896435715732916841648752931");
+		
+		assertEquals("String should've been wrong", false, result);
+	
+	}
+	
+	@Test
+	public void testVerifyStringSucceed() {
+		SudokuVerifier verifier = new SudokuVerifier();
+		
+		boolean result = verifier.verifyString("417369825632158947958724316825437169791586432346912758289643571573291684164875293");
+		
+		assertEquals("String should've been right", true, result);
+	
+	}
+	
+	@Test
+	public void testVerifyStringZero() {
+		SudokuVerifier verifier = new SudokuVerifier();
+		
+		boolean result = verifier.verifyString("417369825632158947950724316825437169791586432346912758289643571573291684164875293");
+		
+		assertEquals("String should've been wrong", false, result);
+	}
+	
+	@Test
+	public void testSudokuMatrix() {
+		SudokuVerifier verifier = new SudokuVerifier();
+		
+		verifier.buildMatrix("417369825632158947958724316825437169791586432346912758289643571573291684164875293");
+		
+		int[][] matrix = verifier.getSudokuMatrix();
+		
+		assertEquals("String should've been right", 9, matrix.length);
+		assertEquals("String should've been right", 9, matrix[0].length);
+	
+	}
+	
 
 }
