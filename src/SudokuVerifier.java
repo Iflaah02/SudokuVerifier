caeb33d0f61577c9b0e020ed637112e3beebc76f
Villek <vilkempp@student.oulu.fi>
2017-09-27 16:47:04 +0300

    beginning

--- src/SudokuVerifier.java
+++ src/SudokuVerifier.java
@@ -1,10 +1,20 @@
 
 public class SudokuVerifier {
-	
+	public int return_code;
 	public int verify(String candidateSolution) {
 		// returns 0 if the candidate solution is correct
+		return_code=valid_size(candidateSolution);
 		return 0;
 		
 		//check
 	}
+	
+	public int valid_size(String s) {
+		if (s.length()!=(9*9)){
+			return 1;
+		}
+		return 0;
+	}
 }
+
+
--- tests/SudokuVerifierParametrizedTest.java
+++ tests/SudokuVerifierParametrizedTest.java
@@ -5,8 +5,30 @@ import org.junit.Test;
 public class SudokuVerifierParametrizedTest {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void test_valid_size_fail() {
+		SudokuVerifier sudoku = new SudokuVerifier();
+		
+		int code = sudoku.valid_size("12345678910");
+		assertEquals(1,code);
+		
 	}
 
+	@Test
+	public void test_valid_size_pass() {
+		SudokuVerifier sudoku = new SudokuVerifier();
+		StringBuilder stringBuilder = new StringBuilder();
+
+		
+
+
+		
+		for(int i =0;i<81;i++) {
+			
+			stringBuilder.append("1");
+		}
+		String finalString = stringBuilder.toString();
+		int code = sudoku.valid_size(finalString);
+		assertEquals(0,code);
+		
+	}
 }
