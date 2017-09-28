99e685ec1a0f2641a11f3490eadddc5a3adc4f32
molko <molkoback@gmail.com>
2017-09-28 19:28:19 +0300

    Assignment Finished

--- src/SudokuVerifier.java
+++ src/SudokuVerifier.java
@@ -1,10 +1,63 @@
-
 public class SudokuVerifier {
-	
 	public int verify(String candidateSolution) {
-		// returns 0 if the candidate solution is correct
+		for (int i = 0; i < 9; i++) {
+			int ret;
+			if(
+				(ret = verify_grid(candidateSolution, i)) < 0 ||
+				(ret = verify_row(candidateSolution, i)) < 0 ||
+				(ret = verify_column(candidateSolution, i)) < 0
+			) {
+				return ret;
+			}
+		}
 		return 0;
-		
-		//check
+	}
+	
+	public int verify_set(String set) {
+		for (int i = 0; i < 9; i++) {
+			char c = set.charAt(i);
+			if (c < '1' || c > '9') {
+				return -1;
+			}
+			else if (!set.contains(Integer.toString(i+1))) {
+				return -2;
+			}
+		}
+		return 0;
+	}
+	
+	public int verify_grid(String candidateSolution, int grid) {
+		String set = "";
+		for (int i = 0; i < 3; i++) {
+			for (int j = 0; j < 3; j++) {
+				set += candidateSolution.charAt(
+						grid/3*27 + grid%3*3 + i*9 + j
+				);
+			}
+		}
+		int ret = verify_set(set);
+		return ret;
+	}
+	
+	public int verify_row(String candidateSolution, int row) {
+		String set = "";
+		for (int i = 0; i < 9; i++) {
+			set += candidateSolution.charAt(
+					row*9 + i
+			);
+		}
+		int ret = verify_set(set);
+		return ret == -2 ? -3 : ret;
+	}
+	
+	public int verify_column(String candidateSolution, int column) {
+		String set = "";
+		for (int i = 0; i < 9; i++) {
+			set += candidateSolution.charAt(
+					i*9 + column
+			);
+		}
+		int ret = verify_set(set);
+		return ret == -2 ? -4 : ret;
 	}
 }
--- tests/SudokuVerifierTest.java
+++ tests/SudokuVerifierTest.java
@@ -7,9 +7,134 @@ public class SudokuVerifierTest {
 // A correct Sudoku string: 417369825632158947958724316825437169791586432346912758289643571573291684164875293
 // An incorrect Sudoku string: 123456789912345678891234567789123456678912345567891234456789123345678912234567891
 
+	public String sudoku_valid = "417369825632158947958724316825437169791586432346912758289643571573291684164875293";	
+	
 	@Test
-	public void testVerify() {
-		fail("Not yet implemented");
+	public void test_verify_set_valid() {
+		SudokuVerifier sv = new SudokuVerifier();
+		assertEquals(
+			"Verification wasn't true",
+			sv.verify_set("987654321"),
+			0
+		);
 	}
-
+	
+	@Test
+	public void test_verify_set_invalidchar() {
+		SudokuVerifier sv = new SudokuVerifier();
+		assertEquals(
+			"Verification wasn't false",
+			sv.verify_set("907654321"),
+			-1
+		);
+	}
+	
+	@Test
+	public void test_verify_set_notin() {
+		SudokuVerifier sv = new SudokuVerifier();
+		assertEquals(
+			"Verification wasn't false",
+			sv.verify_set("997654321"),
+			-2
+		);
+	}
+	
+	@Test
+	public void test_verify_grid_valid() {
+		SudokuVerifier sv = new SudokuVerifier();
+		assertEquals(
+			"Verification wasn't 0",
+			sv.verify_grid(this.sudoku_valid, 5),
+			0
+		);
+	}
+	
+	@Test
+	public void test_verify_grid_invalid() {
+		SudokuVerifier sv = new SudokuVerifier();
+		assertEquals(
+			"Verification wasn't -2",
+			sv.verify_grid(
+				"123456789912345678891234567789123456678912345567891234456789123345678912234567891",
+				5
+			),
+			-2
+		);
+	}
+	
+	@Test
+	public void test_verify_row_valid() {
+		SudokuVerifier sv = new SudokuVerifier();
+		assertEquals(
+			"Verification wasn't 0",
+			sv.verify_row(this.sudoku_valid, 5),
+			0
+		);
+	}
+	
+	@Test
+	public void test_verify_row_invalid() {
+		SudokuVerifier sv = new SudokuVerifier();
+		assertEquals(
+			"Verification wasn't -3",
+			sv.verify_row(
+				"123456789912345678881234567789123456678912345567891234456789123345678912234567891",
+				2
+			),
+			-3
+		);
+	}
+	
+	@Test
+	public void test_verify_column_valid() {
+		SudokuVerifier sv = new SudokuVerifier();
+		assertEquals(
+			"Verification wasn't 0",
+			sv.verify_column(this.sudoku_valid, 5),
+			0
+		);
+	}
+	
+	@Test
+	public void test_verify_column_invalid() {
+		SudokuVerifier sv = new SudokuVerifier();
+		assertEquals(
+			"Verification wasn't -4",
+			sv.verify_column(
+				"223456789912345678881234567789123456678912345567891234456789123345678912234567891",
+				0
+			),
+			-4
+		);
+	}
+	
+	@Test
+	public void test_verify_valid() {
+		SudokuVerifier sv = new SudokuVerifier();
+		assertEquals(
+			"Verification wasn't 0",
+			sv.verify(this.sudoku_valid),
+			0
+		);
+	}
+	
+	@Test
+	public void test_verify_invalid() {
+		SudokuVerifier sv = new SudokuVerifier();
+		assertEquals(
+			"Verification wasn't -2",
+			sv.verify("123456789912345678891234567789123456678912345567891234456789123345678912234567891"),
+			-2
+		);
+	}
+	
+	@Test
+	public void test_verify_invalidchar() {
+		SudokuVerifier sv = new SudokuVerifier();
+		assertEquals(
+			"Verification wasn't -1",
+			sv.verify("-23456789912345678891234567789123456678912345567891234456789123345678912234567891"),
+			-1
+		);
+	}
 }
