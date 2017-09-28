f489627a1dad14911744dca3908ec9a883d86ae0
ipuhakka <ipuhakka@ts137-pc25.student.yo.oulu.fi>
2017-09-28 15:49:23 +0300

    assignment

--- src/SudokuVerifier.java
+++ src/SudokuVerifier.java
@@ -3,8 +3,44 @@ public class SudokuVerifier {
 	
 	public int verify(String candidateSolution) {
 		// returns 0 if the candidate solution is correct
+		//check for rule 1 
+		char[] charSolution = candidateSolution.toCharArray();
+		for (int i = 0; i < charSolution.length; i++) {
+			if (Character.getNumericValue(charSolution[i]) < 1) {
+				return -1;
+			}
+		}
+		//create 9x9 table
+		int[][] table = new int[9][9];
+		int currentNumber = 0;
+		for (int col = 0; col < 9; col++) {
+			
+			for (int row = 0; row < 9; row++) {
+				table[col][row] = Character.getNumericValue(charSolution[currentNumber]);
+				currentNumber++;
+			}
+		}
+		
+		//check for rule 3 (rows):
+		checkRowViolation(table);
+		
 		return 0;
 		
 		//check
 	}
+	
+	public int checkRowViolation(int[][] table) {
+		//rule 3
+		for (int col = 0; col < 9; col++) {
+			for (int rowItem = 0; rowItem < 9; rowItem++) {
+				for (int compItem = 0; compItem < 9; compItem++) {
+					if (table[col][rowItem] == table[col][compItem] && rowItem != compItem) {
+						return -3;
+					}
+				}
+			}
+		}
+		return 0;
+	}
+	
 }
--- tests/SudokuVerifierTest.java
+++ tests/SudokuVerifierTest.java
@@ -7,9 +7,32 @@ public class SudokuVerifierTest {
 // A correct Sudoku string: 417369825632158947958724316825437169791586432346912758289643571573291684164875293
 // An incorrect Sudoku string: 123456789912345678891234567789123456678912345567891234456789123345678912234567891
 
+	
 	@Test
-	public void testVerify() {
-		fail("Not yet implemented");
+	public void testrule1() {
+		SudokuVerifier obj = new SudokuVerifier();
+		int ret = obj.verify("4634-6346464363464343643433366446666");
+		
+		assertEquals(-1, ret);
+		
 	}
+	
+	@Test
+	public void testrule3() {
+		SudokuVerifier obj = new SudokuVerifier();
+		int ret = obj.verify("4173698256321589479587243168254371697915864323469127582896435715732916841" + 
+				"64875293");
+		
+		assertEquals(0, ret);
+		
+		ret = obj.verify("44444444444444444444444444444444444444444444444444444444444444444444444444444" + 
+				"44444444");
+		
+		assertEquals(-3, ret);
+		
+		
+	}
+	
+	
 
 }
