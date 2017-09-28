b60c7c553342948b6e580a25bc11387e71efaabf
dtoniuc <dtoniuc@ts137-pc2.student.yo.oulu.fi>
2017-09-28 14:39:45 +0300

    ADD: String length validation

--- src/SudokuVerifier.java
+++ src/SudokuVerifier.java
@@ -1,10 +1,30 @@
 
 public class SudokuVerifier {
 	
+	private int rows;
+	private int cols;
+	private int squareDim;
+	
+	public SudokuVerifier(int rows, int cols, int squareDim) {
+		this.rows = rows;
+		this.cols = cols;
+		this.squareDim = squareDim;
+	}
+	
 	public int verify(String candidateSolution) {
-		// returns 0 if the candidate solution is correct
-		return 0;
+		if(hasInvalidNumberOfCharacters(candidateSolution))
+			return -1;
 		
-		//check
+		return 0;
+	}
+	
+	private boolean hasInvalidNumberOfCharacters(String string) {
+		int expectedStringLength = rows * cols;
+		
+		if(string.length() != expectedStringLength) {
+			return true;
+		}
+		
+		return false;
 	}
 }
--- tests/SudokuVerifierTest.java
+++ tests/SudokuVerifierTest.java
@@ -8,8 +8,45 @@ public class SudokuVerifierTest {
 // An incorrect Sudoku string: 123456789912345678891234567789123456678912345567891234456789123345678912234567891
 
 	@Test
-	public void testVerify() {
-		fail("Not yet implemented");
+	public void testVerify_twoShortString_returnMinusOne() {
+		//init
+		SudokuVerifier verifier = new SudokuVerifier(9, 9, 3);
+		String incorectString = "4173698256321589479587243168254371697915864323469127582896435715732916841648752";
+		
+		//Act
+		int result = verifier.verify(incorectString);
+		
+		//Assert
+		int expected = -1;
+		assertEquals(expected, result);
+	}
+	
+	@Test
+	public void testVerify_twoLongString_returnMinusOne() {
+		//init
+		SudokuVerifier verifier = new SudokuVerifier(9, 9, 3);
+		String incorectString = "41736982563215894795872431682543716979158643234691275828964357157329168416487529313458";
+		
+		//Act
+		int result = verifier.verify(incorectString);
+		
+		//Assert
+		int expected = -1;
+		assertEquals(expected, result);
+	}
+	
+	@Test
+	public void testVerify_correctString_zero() {
+		//init
+		SudokuVerifier verifier = new SudokuVerifier(9, 9, 3);
+		String incorectString = "417369825632158947958724316825437169791586432346912758289643571573291684164875293";
+		
+		//Act
+		int result = verifier.verify(incorectString);
+		
+		//Assert
+		int expected = 0;
+		assertEquals(expected, result);
 	}
 
 }
