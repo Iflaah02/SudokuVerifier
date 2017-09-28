2c309ee064b24bcd709b06e322bd068f4efe6be1
Miikka <miklpr@outlook.com>
2017-09-28 13:25:14 +0300

    Assignment Finished

--- src/SudokuVerifier.java
+++ src/SudokuVerifier.java
@@ -3,8 +3,27 @@ public class SudokuVerifier {
 	
 	public int verify(String candidateSolution) {
 		// returns 0 if the candidate solution is correct
+		
 		return 0;
 		
 		//check
 	}
-}
+	
+	public int checkLenght(String candidateSolution) {
+		int solutionLenght = candidateSolution.length();
+		
+		if (solutionLenght > 81) {
+			return -5;
+		} else {
+			return 0;
+		}
+	}
+	
+	public String[] sliceString(String candidateSolution) {
+		String[] rows = {};
+		for (int i = 0; i < candidateSolution.length(); i += 10) {
+			rows[i] = candidateSolution.substring(i, Math.min(i + 10, candidateSolution.length()));
+			}
+		return rows;
+	}
+ }
--- tests/SudokuVerifierTest.java
+++ tests/SudokuVerifierTest.java
@@ -11,5 +11,31 @@ public class SudokuVerifierTest {
 	public void testVerify() {
 		fail("Not yet implemented");
 	}
+	
+	@Test
+	public void testLengthVerifierWithCorrectLenght() {
+		SudokuVerifier sVer = new SudokuVerifier();
+		String str = "417369825632158947958724316825437169791586432346912758289643571573291684164875293";
+		
+		assertEquals(0, sVer.checkLenght(str));
+	}
+	
+	@Test
+	public void testLengthVerifierWithIncorrectLenght() {
+		SudokuVerifier sVer = new SudokuVerifier();
+		String str = "1234567899123456788912345677891234566789123455678912344567891233456789122345678911234";
+		
+		assertEquals(-5, sVer.checkLenght(str));
+	}
+	
+	@Test
+	public void testSliceStringCorrectString() {
+		SudokuVerifier sVer = new SudokuVerifier();
+		String str = "417369825632158947958724316825437169791586432346912758289643571573291684164875293";
+		
+		String[] compareTo = {"417369825","632158947","958724316","825437169","791586432","346912758","289643571","573291684","164875293"};
+		
+		assertEquals(compareTo, sVer.sliceString(str));
+	}
 
 }
