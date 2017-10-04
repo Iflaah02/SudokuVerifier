3d3dd61e4b0c96dec4afa1db89590445c9e4e596
Villek <vilkempp@student.oulu.fi>
2017-10-04 16:45:21 +0300

    almost done

--- src/SudokuVerifier.java
+++ src/SudokuVerifier.java
@@ -3,7 +3,16 @@ public class SudokuVerifier {
 	public int return_code;
 	public int verify(String candidateSolution) {
 		// returns 0 if the candidate solution is correct
-		return_code=valid_size(candidateSolution);
+		
+		char[] char_array = candidateSolution.toCharArray();
+		
+		
+		if(valid_size(candidateSolution)==1 || contain_int(candidateSolution) == 1) {
+		return 1;	
+		}
+		
+		check_rows(candidateSolution);
+		check_columns(candidateSolution);
 		return 0;
 		
 		//check
@@ -15,6 +24,88 @@ public class SudokuVerifier {
 		}
 		return 0;
 	}
+	
+	public int contain_int(String s) {
+		int range=s.length();		
+		for(int i=0;i<range;i++) {
+			if(!Character.isDigit(s.charAt(i))){
+			return 1;
+			}
+		}
+		
+		return 0;
+	}
+	
+	public int check_rows(String s) {
+		char[] row = new char[9];
+		for(int y=0;y<81;y=y+9) {
+			for(int x=0;x<9;x++) {
+				row[x]=s.charAt(x+y);
+				}
+			if(validate(row)==1) {
+				return -3;
+			}
+		}
+		
+		return 0;
+	}
+	
+	public int check_columns(String s) {
+		char[] row = new char[9];
+		for(int y=0;y<9;y=y+1) {
+			for(int x=0+y;x<9+y;x=x+9) {
+				row[x]=s.charAt(x+y);
+				}
+			if(validate(row)==1) {
+				return -3;
+			}
+		}
+		
+		return 0;
+	}
+	
+	public int validate(char[] c) {
+		char[] values = {'1','0','0','0','0','0','0','0','0','0'};
+		
+		for(char s : c) {
+			if(s=='1') {
+				values[1]='1';
+			}
+			if(s=='2') {
+				values[2]='1';
+			}
+			if(s=='3') {
+				values[3]='1';
+			}
+			if(s=='4') {
+				values[4]='1';
+			}
+			if(s=='5') {
+				values[5]='1';
+			}
+			if(s=='6') {
+				values[6]='1';
+			}
+			if(s=='7') {
+				values[7]='1';
+			}
+			if(s=='8') {
+				values[8]='1';
+			}
+			if(s=='9') {
+				values[9]='1';
+			}
+
+			}
+		for(char s : values) {
+			if(s!='1') {
+				return 1;
+			}
+			
+		}
+		return 0;
+	}
+	
 }
 
 
--- tests/SudokuVerifierParametrizedTest.java
+++ tests/SudokuVerifierParametrizedTest.java
@@ -8,7 +8,7 @@ public class SudokuVerifierParametrizedTest {
 	public void test_valid_size_fail() {
 		SudokuVerifier sudoku = new SudokuVerifier();
 		
-		int code = sudoku.valid_size("12345678910");
+		int code = sudoku.valid_size("12345");
 		assertEquals(1,code);
 		
 	}
@@ -17,10 +17,6 @@ public class SudokuVerifierParametrizedTest {
 	public void test_valid_size_pass() {
 		SudokuVerifier sudoku = new SudokuVerifier();
 		StringBuilder stringBuilder = new StringBuilder();
-
-		
-
-
 		
 		for(int i =0;i<81;i++) {
 			
@@ -31,4 +27,56 @@ public class SudokuVerifierParametrizedTest {
 		assertEquals(0,code);
 		
 	}
+	
+	@Test
+	public void test_contain_int() {
+		SudokuVerifier sudoku = new SudokuVerifier();
+		StringBuilder stringBuilder = new StringBuilder();
+		
+		for(int i =0;i<81;i++) {
+			
+			stringBuilder.append("3");
+		}
+		String finalString = stringBuilder.toString();
+		
+		int code = sudoku.contain_int(finalString);
+		assertEquals(0,code);
+
+		StringBuilder stringBuilder2 = new StringBuilder();
+		
+		for(int i =0;i<81;i++) {
+			
+			stringBuilder2.append("A");
+		}
+		String finalString2 = stringBuilder2.toString();
+		code = sudoku.contain_int(finalString2);
+		assertEquals(1,code);
+	}
+	
+	@Test
+	public void test_validate() {
+		SudokuVerifier sudoku = new SudokuVerifier();
+
+		char[] c = {'3','2','1','4','5','9','7','8','6'};
+		int score =sudoku.validate(c);
+		assertEquals(0,score);
+		
+		char[] c2 = {'2','2','1','4','5','9','7','8','6'};
+		score =sudoku.validate(c2);
+		assertEquals(1,score);
+	}
+	
+	@Test
+	public void test_check_rows() {
+		SudokuVerifier sudoku = new SudokuVerifier();
+		String correct_s = "417369825632158947958724316825437169791586432346912758289643571573291684164875293";
+	    String incorrect_s = "123456789912345678891234567789123456618912345567891234456789123345678912234567891";
+		
+		int score =sudoku.check_rows(correct_s);
+		assertEquals(0,score);
+		
+		score =sudoku.check_rows(incorrect_s);
+		assertEquals(-3,score);
+	}	
+	
 }
