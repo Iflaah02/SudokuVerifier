139e86bec3742534e6545ecc59b66e1123d4d0c4
dachen <dachen@MIKROTILAT1-83.student.yo.oulu.fi>
2017-10-05 14:57:08 +0300

    implemented rule 1 and 3.

--- src/SudokuVerifier.java
+++ src/SudokuVerifier.java
@@ -1,10 +1,120 @@
+import java.util.HashSet;
 
 public class SudokuVerifier {
+	private String[] globalRows = new String[9];
+	private String[] globalCols = new String[9];
+	private String[] subGrids = new String[9];
 	
 	public int verify(String candidateSolution) {
-		// returns 0 if the candidate solution is correct
-		return 0;
+		globalRows = extractGlobalRows(candidateSolution);
+		int verifiedResult = 0;
+		//slice 1: A cell in a Sudoku game can only store positive digits, i.e. 1...9.
+		if(!containsOnlyPositiveDigits(candidateSolution)) {
+			return verifiedResult = -1;
+		}
+		//slice 2:  All digits appear only once in a global row, i.e. they cannot repeat
+		if(digitRepeatedInGlobalRow(globalRows)) {
+			return verifiedResult = -3;
+		}
+//		//slice 3: All digits appear only once in a global column, i.e. they cannot repeat
+//		if(digitRepeatedInGlobalCol(candidateSolution)) {
+//			return verifiedResult = -4;
+//		}
+		return verifiedResult;
+	}
+	private String[] extractGlobalRows(String sudokuSolution) {
+		String[] rows = new String[9];
+		int range = 9;
+		int start, end;
+		for(int i = 0; i < 9; i++) {
+			start = range * i;
+			end = start + 9;
+			rows[i] = sudokuSolution.substring(start, end);
+			System.out.println(rows[i]);
+		}
+		return rows;
+	}
+	//slice 1: implement containsOnlyPositiveDigits method
+	private boolean containsOnlyPositiveDigits(String sudokuSolution) {
+		boolean hasPositiveDigitsOnly = true;
+		for(int i = 0; i < sudokuSolution.length(); i++) {
+			String aStrChar = Character.toString(sudokuSolution.charAt(i));
+			try {
+				Integer.parseInt(aStrChar);
+			}catch(NumberFormatException e) {
+				hasPositiveDigitsOnly = false;
+			}
+		}
+		return hasPositiveDigitsOnly;
+	}
+	//slice 2: implement digitRepeatedInGlobalRow method
+	private boolean digitRepeatedInGlobalRow(String[] sudokuSolution) {
+		boolean isRepeated = false;
+		for(int i = 0; i < sudokuSolution.length; i++) {
+			char[] row = sudokuSolution[i].toCharArray();
+			HashSet<Character> h = new HashSet<>();
+			nestedLoop:
+			for(int j = 0; j < row.length; j++) {
+				char c = row[j];
+				if(h.contains(c)) {
+					isRepeated = true;
+					break nestedLoop;
+				} else {
+					h.add(c);
+				}
+			}
+			if(isRepeated == true)
+				break;
+		}
 		
-		//check
+//		int rowLength = 9;
+//		int startIndex,endIndex;
+//		topLoop:
+//		for(int i = 0; i < 9; i++) {
+//			startIndex = rowLength * i;
+//			endIndex = startIndex + 9;
+//			char[] sudokuRow = sudokuSolution.substring(startIndex, endIndex).toCharArray();
+//			HashSet<Character> h = new HashSet<>();
+//			nestedLoop:
+//			for(int j = 0; j < sudokuRow.length; j++) {
+//				char c = sudokuRow[j];
+//				if(h.contains(c)) {
+//					isRepeated = true;
+//					break nestedLoop;
+//				} else {
+//					h.add(c);
+//				}
+//			}
+//			if(isRepeated == true)
+//				break topLoop;
+//		}
+		return isRepeated;
+	}
+	//slice 3: implement digitRepeatedInGlobalCol method
+	private boolean digitRepeatedInGlobalCol(String sudokuSolution) {
+		boolean isRepeated = false;
+		topLoop:
+		for(int i = 0; i < 9; i++) {
+			char[] sudokuCol = new char[9];
+			int beginningColIndex = i;
+			for(int j = 0; j < 9; j++) {
+				sudokuCol[j] = sudokuSolution.charAt(beginningColIndex);
+				beginningColIndex = beginningColIndex + 9;
+			}
+			HashSet<Character> h = new HashSet<>();
+			nestedLoop:
+			for(int k = 0; k < sudokuCol.length; k++) {
+				char c = sudokuCol[k];
+				if(h.contains(c)) {
+					isRepeated = true;
+					break nestedLoop;
+				} else {
+					h.add(c);
+				}
+			}
+			if(isRepeated == true)
+				break topLoop;
+		}
+		return isRepeated;
 	}
 }
--- tests/SudokuVerifierTest.java
+++ tests/SudokuVerifierTest.java
@@ -1,15 +1,39 @@
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class SudokuVerifierTest {
 
 // A correct Sudoku string: 417369825632158947958724316825437169791586432346912758289643571573291684164875293
 // An incorrect Sudoku string: 123456789912345678891234567789123456678912345567891234456789123345678912234567891
-
-	@Test
-	public void testVerify() {
-		fail("Not yet implemented");
+	String correctSudoku, incorrectSudoku;
+	SudokuVerifier sVerifier;
+	@Before
+	public void setUp() {
+		correctSudoku = "417369825632158947958724316825437169791586432346912758289643571573291684164875293";
+		//incorrectSudoku = "123456789912345678891234567789123456678912345567891234456789123345678912234567891";
+		sVerifier = new SudokuVerifier();
 	}
+	
+
+//	//Slice 1: test containsOnlyPositiveDigits method
+	@Test
+	public void test_containsLetter_a() {
+		incorrectSudoku = "a23456789912345678891234567789123456678912345567891234456789123345678912234567891";
+		assertEquals("-1 is returned if rule one is broken", -1, sVerifier.verify(incorrectSudoku));
+	}
+	//Slice 2: test digitRepeatedInGlobalRow method
+	@Test
+	public void test_digit_repeated_in_a_global_row() {
+		incorrectSudoku = "447369825632158947958724316825437169791586432346912758289643571573291684164875293";
+		assertEquals("-3 is returned if rule three is broken", -3, sVerifier.verify(incorrectSudoku));
+	}
+	//Slice 3: test digitRepeatedInGlobalCol method
+//	@Test
+//	public void test_digit_repeated_in_a_global_column() {
+//		incorrectSudoku = "417369825632158947958724316825437169791586432346912758289643571573291684164875295";
+//		assertEquals("-4 is returned if rule four is broken", -4, sVerifier.verify(incorrectSudoku));
+//	}
 
 }
