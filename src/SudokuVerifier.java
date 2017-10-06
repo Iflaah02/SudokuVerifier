0b2226b0d2855ecc699e3d5938b4971822f0e80f
vilkempp <vilkempp@ts135-pc9.student.yo.oulu.fi>
2017-10-06 14:03:33 +0300

    Refactoring done

--- src/SudokuVerifier.java
+++ src/SudokuVerifier.java
@@ -1,21 +1,31 @@
 
+//Sudoku verifier done while utilizing refactoring methods
+//JDoedorant still finds several long methods due to for loops in the column, row and subgrid checks. Refactoring them did not seem like
+//it would help the readability of the code so I left them as is. 
 public class SudokuVerifier {
-	public int return_code;
+	public int return_code=0;
 	public int verify(String candidateSolution) {
-		// returns 0 if the candidate solution is correct
-		
-		char[] char_array = candidateSolution.toCharArray();
-		
-		
+
+			
 		if(valid_size(candidateSolution)==1 || contain_int(candidateSolution) == 1) {
-		return 1;	
+		return -1;	
 		}
 		
-		check_rows(candidateSolution);
-		check_columns(candidateSolution);
+		return_code=check_rows(candidateSolution);
+		if(return_code!=0) {
+			return return_code;
+		}
+		return_code=check_columns(candidateSolution);
+		if(return_code!=0) {
+			return return_code;
+		}
+		return_code=check_subgrids(candidateSolution);
+		if(return_code!=0) {
+			return return_code;
+		}
 		return 0;
 		
-		//check
+	
 	}
 	
 	public int valid_size(String s) {
@@ -51,52 +61,47 @@ public class SudokuVerifier {
 	}
 	
 	public int check_columns(String s) {
-		char[] row = new char[9];
+		char[] row = new char[10];
+		int counter=1;
 		for(int y=0;y<9;y=y+1) {
-			for(int x=0+y;x<9+y;x=x+9) {
-				row[x]=s.charAt(x+y);
+			for(int x=y;x<81;x=x+9) {
+				row[counter]=s.charAt(x);
+				counter++;
 				}
+			counter=1;
 			if(validate(row)==1) {
-				return -3;
+				return -4;
 			}
 		}
 		
 		return 0;
 	}
 	
-	public int validate(char[] c) {
-		char[] values = {'1','0','0','0','0','0','0','0','0','0'};
+	public int check_subgrids(String s) {
+		int counter=1;
+		char[] row = new char[10];
+		for(int sty=0;sty<80;sty=sty+27) {
+			for(int stx=0;stx<9;stx=stx+3) {
+				for(int i=0;i<24;i=i+9) {
+					for(int j=0;j<3;j++) {
+						row[counter]=s.charAt(sty+stx+i+j);
+						counter++;
+					}
+					}
+			
+			if(validate(row)==1) {
+				return -2;
+			}
+			
+			counter=1;
+		}
+		}
 		
-		for(char s : c) {
-			if(s=='1') {
-				values[1]='1';
-			}
-			if(s=='2') {
-				values[2]='1';
-			}
-			if(s=='3') {
-				values[3]='1';
-			}
-			if(s=='4') {
-				values[4]='1';
-			}
-			if(s=='5') {
-				values[5]='1';
-			}
-			if(s=='6') {
-				values[6]='1';
-			}
-			if(s=='7') {
-				values[7]='1';
-			}
-			if(s=='8') {
-				values[8]='1';
-			}
-			if(s=='9') {
-				values[9]='1';
-			}
-
-			}
+		return 0;
+	}
+	//refactoring extract method
+	public int validate(char[] c) {
+		char[] values = values(c);
 		for(char s : values) {
 			if(s!='1') {
 				return 1;
@@ -104,6 +109,40 @@ public class SudokuVerifier {
 			
 		}
 		return 0;
+	}
+//refactoring extract method
+	private char[] values(char[] c) {
+		char[] values = { '1', '0', '0', '0', '0', '0', '0', '0', '0', '0' };
+		for (char s : c) {
+			if (s == '1') {
+				values[1] = '1';
+			}
+			if (s == '2') {
+				values[2] = '1';
+			}
+			if (s == '3') {
+				values[3] = '1';
+			}
+			if (s == '4') {
+				values[4] = '1';
+			}
+			if (s == '5') {
+				values[5] = '1';
+			}
+			if (s == '6') {
+				values[6] = '1';
+			}
+			if (s == '7') {
+				values[7] = '1';
+			}
+			if (s == '8') {
+				values[8] = '1';
+			}
+			if (s == '9') {
+				values[9] = '1';
+			}
+		}
+		return values;
 	}
 	
 }
--- tests/SudokuVerifierParametrizedTest.java
+++ tests/SudokuVerifierParametrizedTest.java
@@ -16,13 +16,7 @@ public class SudokuVerifierParametrizedTest {
 	@Test
 	public void test_valid_size_pass() {
 		SudokuVerifier sudoku = new SudokuVerifier();
-		StringBuilder stringBuilder = new StringBuilder();
-		
-		for(int i =0;i<81;i++) {
-			
-			stringBuilder.append("1");
-		}
-		String finalString = stringBuilder.toString();
+		String finalString = build_string_with("1");
 		int code = sudoku.valid_size(finalString);
 		assertEquals(0,code);
 		
@@ -31,24 +25,12 @@ public class SudokuVerifierParametrizedTest {
 	@Test
 	public void test_contain_int() {
 		SudokuVerifier sudoku = new SudokuVerifier();
-		StringBuilder stringBuilder = new StringBuilder();
-		
-		for(int i =0;i<81;i++) {
-			
-			stringBuilder.append("3");
-		}
-		String finalString = stringBuilder.toString();
+		String finalString = build_string_with("1");
 		
 		int code = sudoku.contain_int(finalString);
 		assertEquals(0,code);
 
-		StringBuilder stringBuilder2 = new StringBuilder();
-		
-		for(int i =0;i<81;i++) {
-			
-			stringBuilder2.append("A");
-		}
-		String finalString2 = stringBuilder2.toString();
+		String finalString2 = build_string_with("A");
 		code = sudoku.contain_int(finalString2);
 		assertEquals(1,code);
 	}
@@ -77,6 +59,59 @@ public class SudokuVerifierParametrizedTest {
 		
 		score =sudoku.check_rows(incorrect_s);
 		assertEquals(-3,score);
-	}	
+	}
 	
+	@Test
+	public void test_check_columns() {
+		SudokuVerifier sudoku = new SudokuVerifier();
+		String correct_s = "417369825632158947958724316825437169791586432346912758289643571573291684164875293";
+	    String incorrect_s = "123456789912345678891234567789123456618912345567891234456789123345678912234567891";
+		
+		int score =sudoku.check_columns(correct_s);
+		assertEquals(0,score);
+		
+		score =sudoku.check_columns(incorrect_s);
+		assertEquals(-4,score);
+	}
+	
+	@Test
+	public void test_check_subgrids() {
+		SudokuVerifier sudoku = new SudokuVerifier();
+		String correct_s = "417369825632158947958724316825437169791586432346912758289643571573291684164875293";
+	    String incorrect_s = "123456789912345678891234567789123456618912345567891234456789123345678912234567891";
+		
+		int score =sudoku.check_subgrids(correct_s);
+		assertEquals(0,score);
+		
+		score =sudoku.check_subgrids(incorrect_s);
+		assertEquals(-2,score);
+	}
+	
+	
+	@Test
+	public void test_verify() {
+		SudokuVerifier sudoku = new SudokuVerifier();
+		String correct_s = "417369825632158947958724316825437169791586432346912758289643571573291684164875293";
+	    String incorrect_s = "123456789912345678891234567789123456618912345567891234456789123345678912234567891";
+		
+		int score =sudoku.verify(correct_s);
+		assertEquals(0,score);
+		
+		score =sudoku.verify(incorrect_s);
+		assertEquals(-3,score);
+	}
+
+//Refactoring Extract methods
+public String build_string_with(String s) {
+	StringBuilder stringBuilder = new StringBuilder();
+	
+	for(int i =0;i<81;i++) {
+		
+		stringBuilder.append(s);
+	}
+	String finalString = stringBuilder.toString();
+	return finalString;
+}
+
+
 }
