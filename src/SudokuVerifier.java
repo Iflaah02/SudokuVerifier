0622c6eb4a9fdfb5ebc58c5d7164f3e061563035
alfitra.20 <alfitrarahman@gmail.com>
2017-10-06 11:02:04 +0300

    refactor r2

--- src/SudokuVerifier.java
+++ src/SudokuVerifier.java
@@ -1,18 +1,28 @@
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+
 public class SudokuVerifier {
 	
 	public int verify(String candidateSolution) {
-		boolean check_invalid_sub_content = false;
 		boolean check_valid_string = checkValidString(candidateSolution);
 		
 		if (check_valid_string == true){
+			List<String> candidateArray = convertToArray(candidateSolution);
 			
-			String[] candidateArray = splitByNumber(candidateSolution);
-			
-			check_invalid_sub_content = checkInvalidSubContent(candidateArray);
-			
+			boolean check_invalid_sub_content = checkInvalidSubContent(candidateArray);
+		//	boolean check_invalid_row_content = checkInvalidRowContent(candidateArray);
 			if (check_invalid_sub_content == false) {
-				return 0;
+				
+				//if (check_invalid_row_content == false){
+				//	return 0;
+				//}
+				//else{
+					//return 3;
+					return 0;
+				//}
 			}else {
 				return 2;
 			}
@@ -20,49 +30,61 @@ public class SudokuVerifier {
 			return 1;
 		}
 	}
-	private String[] splitByNumber(String s){
-	    int counter = (s.length() / 9) + (s.length() % 9 == 0 ? 0 : 1);
-	    String[] candidateArray = new String[counter];
-	  
-	    for(int i=0;i<counter;i++){
-	        candidateArray[i] = s.substring(i*9, Math.min((i+1)*9-1, s.length()));
-	    }
-	    return candidateArray;
+	public List<String> convertToArray(String candidateSolution){
+		List<String> candidateArray = new ArrayList<String>();
+		int index = 0;
+		while (index < candidateSolution.length()) {
+			candidateArray.add(candidateSolution.substring(index, Math.min(index + 9,candidateSolution.length())));
+		    index += 9;
+		}
+		return candidateArray;
 	}
+
 	public boolean checkValidString(String candidateString) {
 		String regex = "[0-9]+";
 		boolean check = candidateString.matches(regex);
 		return check;
 	}
-	public boolean checkInvalidSubContent(String[] candidateArray) {
+	public boolean checkInvalidSubContent(List<String> candidateArray) {
 		boolean duplicate = false;
-		for (int i=0;i<candidateArray.length;i++) {
-			for(int a=0; a < candidateArray[i].length(); a++) {
-				char[] candidateChar = candidateArray[i].toCharArray();
+		for (int i=0;i<candidateArray.size();i++) {
+				char[] candidateChar = candidateArray.get(i).toCharArray();
 				for(int x=0; x < candidateChar.length; x++) {
-					for (int j = x + 1 ; j < candidateChar.length; j++) {
-						if (candidateChar[x] == (candidateChar[j])) {
-			                   duplicate = true;
+					for (int j = 0 ; j < candidateChar.length; j++) {
+						 if (j!=x && candidateChar[j] == candidateChar[x]){
+						      duplicate=true;
 			          	}
 					}
 				}
-			}
+				candidateChar = null;
 		}
 		return duplicate;
 	}
-//	public boolean checkInvalidRowContent(String[] candidateArray) {
-//		boolean duplicate = false;
-//		for (int i=0;i<candidateArray.length;i++) {
-//			
-//			for(int a=0; a < candidateArray[i].length(); a++) {
-//				char[] candidateChar = candidateArray[i].toCharArray();
-//				for (int j = a + 1 ; j < candidateChar.length; j++) {
-//			          if (candidateChar[a] == (candidateChar[j])) {
-////			                   duplicate = true;
-//			          }
-//			     }
-//			}
-//		}
-//		return duplicate;
-//	}
+	//public boolean checkInvalidRowContent(List<String> candidateArray) {
+	//	boolean duplicate = false;
+	//	int i=0;
+	//	int subContentCounter  = 3;
+	//	List<String> rowArray = new ArrayList<String>();
+	//	while(i<subContentCounter) {
+			
+	//		char[] candidateChar = candidateArray.get(subContentCounter).toCharArray();
+			//String temp = candidateArray.get(i);
+	//		for(int x=0; x < 3; x++) {
+	//			rowArray.add(String.valueOf(candidateChar[x]));
+	//		}
+			
+			//	
+		//			for (int j = 0 ; j < candidateChar.length; j++) {
+		//				 if (j!=x && candidateChar[j] == candidateChar[x]){
+		//					 System.out.println("true");
+		///				      duplicate=true;
+		//	          	}
+		//			}
+		//		}
+			//	candidateChar = null;
+		//	i++;
+		//	subContentCounter +=3;
+		//}
+		//return duplicate;
+	//}
 }
--- tests/SudokuVerifierTest.java
+++ tests/SudokuVerifierTest.java
@@ -20,12 +20,19 @@ public class SudokuVerifierTest {
 		assertEquals("Sudoku", 1, check);
 	}
 	@Test
-	public void check_rule2_grid_test() {
+	public void check_rule2_sub_content_test() {
 		SudokuVerifier verifier = new SudokuVerifier();
-		String test = "123456789912345678891234567789123456678912345567891234456789123345678912234567891";
+		String test = "123556789912345678891234567789123456678912345567891234456789123345678912234567891";
 		int check = verifier.verify(test);
 		assertEquals("Sudoku",2, check);
 	}
+	//@Test
+	//public void check_rule3_row_test() {
+	//	SudokuVerifier verifier = new SudokuVerifier();
+	//	String test = "123456789912345678891234567789123456678912345567891234456789123345678912234567891";
+	//	int check = verifier.verify(test);
+	//	assertEquals("Sudoku",3, check);
+	//}
 	
 
 }
