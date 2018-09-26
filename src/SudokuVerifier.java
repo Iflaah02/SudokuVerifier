ea6d62c659c2dc586b5f68ca3e3012c220955f4d
Sergei Dovzhikov <illidanboss@gmail.com>
2018-09-26 18:53:01 +0300

    assignment in progress

--- src/SudokuVerifier.java
+++ src/SudokuVerifier.java
@@ -1,10 +1,50 @@
+import java.util.Arrays;
 
 public class SudokuVerifier {
 	
+	// Expected appearance of digits
+	private final int[] expected =
+		{1,1,1,1,1,1,1,1,1};
+	
+	// How many times each digit appears
+	private int[] times(String part) {
+		assert(part.length() == 9);
+		int[] res = new int[9];
+		for (int i = 0; i < 9; i++)
+			res[i] = 0;
+		for (int i = 0; i < 9; i++)
+			res[Character.getNumericValue(part.charAt(i)-1)]++;
+		return res; // TODO: Check
+	}
+	
+	private String[] getRows(String board) {
+		String[] res = new String[9];
+		for (int i = 0; i < 9; i++) {
+			res[i] = board.substring(9*i, 9*(i+1));
+		}
+		return res; // TODO: check
+	}
+	
+	private String[] getColums(String board) {
+		// TODO: Implement
+		return null;
+	}
+	
+	private String[] getGrids(String board) {
+		// TODO: Implement
+		return null;
+	}
+	
 	public int verify(String candidateSolution) {
-		// returns 0 if the candidate solution is correct
-		return 0;
-		
-		//check
+		if (candidateSolution.matches("[1-9]{81}")) {
+			// Check rows
+			String[] rows = getRows(candidateSolution);
+			for (int i = 0; i < 9; i++)
+				if (!Arrays.equals(times(rows[i]), expected))
+					return -2;
+			// TODO: Implement
+			return 0;
+		} else
+			return -1;
 	}
 }
--- tests/SudokuVerifierParametrizedTest.java
+++ tests/SudokuVerifierParametrizedTest.java
@@ -1,12 +1,46 @@
 import static org.junit.Assert.*;
 
+import java.util.Arrays;
+import java.util.Collection;
+
+import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 
+@RunWith(Parameterized.class)
 public class SudokuVerifierParametrizedTest {
-
+	
+	private String sudokuString;
+	private int expectedResult;
+	private SudokuVerifier sv;
+	
+	@Before
+	public void init() {
+		sv = new SudokuVerifier();
+	}
+	
+	public SudokuVerifierParametrizedTest(String ss, int er) {
+		this.sudokuString = ss;
+		this.expectedResult = er;
+	}
+	
+	@Parameterized.Parameters(name = "Test #{index}")
+	public static Collection<Object[]> sudokuStrings() {
+		return Arrays.asList(new Object[][] {
+			// Valid string
+			{"417369825632158947958724316825437169791586432346912758289643571573291684164875293", 0},
+			// Less or more characters
+			{"41736982563215894795872431682543716979158643234691275828964357157329168416487529", -1},
+			{"4173698256321589479587243168254371697915864323469127582896435715732916841648752931", -1},
+			// Invalid string
+			{"123456789912345678891234567789123456678912345567891234456789123345678912234567891", -10} // TODO: fix placeholder
+		});
+	}
+	
 	@Test
 	public void test() {
-		fail("Not yet implemented");
+		assertEquals(expectedResult, sv.verify(sudokuString));
 	}
 
 }
