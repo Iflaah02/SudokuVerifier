c5202c646524fd606a300c2287beacb0c84af235
ilundbom <ilundbom@W10-116.student.yo.oulu.fi>
2018-09-26 18:50:53 +0300

    Kesken ja suht pahasti.

--- src/SudokuVerifier.java
+++ src/SudokuVerifier.java
@@ -1,10 +1,60 @@
 
 public class SudokuVerifier {
+	private int[] col = new int[9];
+	private int[][] sudoku = new int[9][9];
 	
 	public int verify(String candidateSolution) {
 		// returns 0 if the candidate solution is correct
+		
+		//table for sudoku solution, should make it easier to go through rows and columns
+		
+		if (sizeChecker(candidateSolution)) {
+			int x = 0;
+			int y = 0;
+			int z = 0;
+			
+			//here we fill the sudoku table with numbers from candidateSolution
+			while (y < 9) {
+				while (x < 9) {
+					char qwer = candidateSolution.charAt(x);
+					z = charToInt(qwer);
+					sudoku[y][x] = z;
+					x++;				
+				}
+				y++;
+			}
+		}
+		else {
+			System.out.println("Wrong sized solution.");
+		}
 		return 0;
 		
 		//check
+	}
+
+	public boolean sizeChecker(String solution) {
+		//System.out.println("length:"+ solution.length());
+		
+		if (solution.length() == 81)
+			return true;
+		else
+			return false;
+	}
+
+	public int charToInt(char abc){	
+		return Character.getNumericValue(abc);
+	}
+	
+	public boolean intChecker(int number) {
+		//here we check, if the int is between 1,2,3,4,5,6,7,8 or 9
+		if (number > 0 && number < 10)
+			return true;
+		else
+			return false;
+	}
+	
+	public boolean columnChecker(int[][] sudoku) {
+		
+		return true;
 	}
 }
--- tests/SudokuVerifierTest.java
+++ tests/SudokuVerifierTest.java
@@ -12,4 +12,99 @@ public class SudokuVerifierTest {
 		fail("Not yet implemented");
 	}
 
+	@Test
+	public void stringSize_too_small() {
+		SudokuVerifier Sf = new SudokuVerifier();
+		boolean test = Sf.sizeChecker("87987");
+		//System.out.println("test:"+test);
+		assertEquals("Should be false", false, test);
+	}
+	
+	@Test
+	public void stringSize_correct() {
+		SudokuVerifier Sf = new SudokuVerifier();
+		boolean test = Sf.sizeChecker("417369825632158947958724316825437169791586432346912758289643571573291684164875293");
+		//System.out.println("test:"+test);
+		assertEquals("Should be true", true, test);
+	}	
+	
+	@Test
+	public void stringSize_too_big() {
+		SudokuVerifier Sf = new SudokuVerifier();
+		boolean test = Sf.sizeChecker("1111417369825632158947958724316825437169791586432346912758289643571573291684164875293");
+		//System.out.println("test:"+test);
+		assertEquals("Should be false", false, test);
+	}	
+	
+	@Test
+	public void intChecker_1(){
+		SudokuVerifier Sf = new SudokuVerifier();
+		boolean test = Sf.intChecker(5);
+		assertEquals("Should be true", true, test);
+	}
+
+	@Test
+	public void intChecker_2(){
+		SudokuVerifier Sf = new SudokuVerifier();
+		boolean test = Sf.intChecker(-1);
+		assertEquals("Should be false", false, test);
+	}
+
+	@Test
+	public void intChecker_3(){
+		SudokuVerifier Sf = new SudokuVerifier();
+		boolean test = Sf.intChecker(0);
+		assertEquals("Should be false", false, test);
+	}	
+	
+	@Test
+	public void intChecker_4(){
+		SudokuVerifier Sf = new SudokuVerifier();
+		boolean test = Sf.intChecker(10);
+		assertEquals("Should be false", false, test);
+	}	
+	
+	@Test
+	public void intChecker_5(){
+		SudokuVerifier Sf = new SudokuVerifier();
+		boolean test = Sf.intChecker(9);
+		assertEquals("Should be True", true, test);
+	}	
+	
+	@Test
+	public void intChecker_6(){
+		SudokuVerifier Sf = new SudokuVerifier();
+		boolean test = Sf.intChecker(1);
+		assertEquals("Should be True", true, test);
+	}	
+	
+	@Test
+	public void testing(){
+		SudokuVerifier Sf = new SudokuVerifier();
+		Sf.verify("12345");
+	}	
+
+	@Test
+	public void char_to_int(){
+		SudokuVerifier Sf = new SudokuVerifier();
+		int x = Sf.charToInt('9');
+		assertEquals("Should be 9", 9, x);
+	}		
+	
+	@Test
+	public void char_to_int_2(){
+		SudokuVerifier Sf = new SudokuVerifier();
+		int x = Sf.charToInt('5');
+		assertEquals("Should be 5", 5, x);
+	}		
+
+	@Test
+	public void verify_1() {
+		SudokuVerifier Sf = new SudokuVerifier();
+		int test = Sf.verify("417369825632158947958724316825437169791586432346912758289643571573291684164875293");
+		//System.out.println("test:"+test);
+		assertEquals("Should be 0 as it does nothing", 0, test);
+	}		
+	
 }
+
