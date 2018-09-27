44337ec428816b6e084437736860b3e40d57ece9
bmustone <bmustone@W10-24.student.yo.oulu.fi>
2018-09-27 18:48:46 +0300

    Work In Progress

--- src/SudokuVerifier.java
+++ src/SudokuVerifier.java
@@ -1,10 +1,59 @@
+import java.util.ArrayList;
+import java.util.List;
 
 public class SudokuVerifier {
 	
 	public int verify(String candidateSolution) {
-		// returns 0 if the candidate solution is correct
-		return 0;
+		if((candidateSolution.length() != 81) || (candidateSolution.contains("-"))) {
+			return -1;
+		}
 		
-		//check
+		
+		return 0;
 	}
+	
+	List<String> getRows(String candidateSolution){
+		List<String> rows = new ArrayList<String>();
+		
+		for(int i=0; i<candidateSolution.length(); i+=9) {
+			rows.add(candidateSolution.substring(i, Math.min(candidateSolution.length(), i + 9)));
+		}
+		
+		return rows;
+	}
+	
+	List<String> getColumns(String candidateSolution){
+		String column;
+		List<String> columns = new ArrayList<String>();
+		
+		for(int i=0; i<9; i++) {
+			column = "";
+			for(int j=i; j<candidateSolution.length();j+=9) {
+				column = column + candidateSolution.substring(j, j+1);
+			}
+			columns.add(column);
+		}
+		
+		return columns;
+	}
+	
+	List<String> getSubGrids(String candidateSolution){
+		String subGrid;
+		List<String> subGrids = new ArrayList<String>();
+		
+		for(int i=0; i<candidateSolution.length(); i+=27) {
+			for(int j=i; j<9; j+=3) {
+				subGrid = "";
+				for(int k=j; k<k+18;k+=9) {
+					for(int l=k; l<l+3;l++) {
+						subGrid = subGrid + candidateSolution.substring(l, l+1);
+					}
+				}
+				subGrids.add(subGrid);
+			}
+		}
+		
+		return subGrids;
+	}
+	
 }
--- tests/SudokuVerifierTest.java
+++ tests/SudokuVerifierTest.java
@@ -1,5 +1,7 @@
 import static org.junit.Assert.*;
 
+import java.util.List;
+
 import org.junit.Test;
 
 public class SudokuVerifierTest {
@@ -8,8 +10,96 @@ public class SudokuVerifierTest {
 // An incorrect Sudoku string: 123456789912345678891234567789123456678912345567891234456789123345678912234567891
 
 	@Test
-	public void testVerify() {
-		fail("Not yet implemented");
+	public void testVerify_ValidString() {
+		// Arrange
+		SudokuVerifier verifier = new SudokuVerifier();
+		int result = 0;
+				
+		// Act
+		result = verifier.verify("417369825632158947958724316825437169791586432346912758289643571573291684164875293");
+				
+		// Assert
+		assertEquals(0, result);
+	}
+	
+	@Test
+	public void testVerify_NotEnoughCharacters() {
+		// Arrange
+		SudokuVerifier verifier = new SudokuVerifier();
+		int result = 0;
+				
+		// Act
+		result = verifier.verify("3124");
+				
+		// Assert
+		assertEquals(-1, result);
 	}
 
+	@Test
+	public void testVerify_TooManyCharacters() {
+		// Arrange
+		SudokuVerifier verifier = new SudokuVerifier();
+		int result = 0;
+				
+		// Act
+		result = verifier.verify("4173698256321589479587243168254371697915864323469127582896435715732916841648752932");
+				
+		// Assert
+		assertEquals(-1, result);
+	}
+	
+	@Test
+	public void testVerify_NegativeNumbers() {
+		// Arrange
+		SudokuVerifier verifier = new SudokuVerifier();
+		int result = 0;
+				
+		// Act
+		result = verifier.verify("4173698256321589479587243168254371697915864323469127582896435715732916841648752-9");
+				
+		// Assert
+		assertEquals(-1, result);
+	}
+	
+	@Test
+	public void testGetRows_2ndRow() {
+		// Arrange
+		SudokuVerifier verifier = new SudokuVerifier();
+		String result = "0";
+				
+		// Act
+		List<String> rows = verifier.getRows("417369825632158947958724316825437169791586432346912758289643571573291684164875293");
+		result = rows.get(1);
+		
+		// Assert
+		assertEquals("632158947", result);
+	}
+	
+	@Test
+	public void testGetColumns_2ndColumn() {
+		// Arrange
+		SudokuVerifier verifier = new SudokuVerifier();
+		String result = "0";
+				
+		// Act
+		List<String> columns = verifier.getColumns("417369825632158947958724316825437169791586432346912758289643571573291684164875293");
+		result = columns.get(1);
+		
+		// Assert
+		assertEquals("135294876", result);
+	}
+	
+	@Test
+	public void testGetSubGrids_2ndSubGrid() {
+		// Arrange
+		SudokuVerifier verifier = new SudokuVerifier();
+		String result = "0";
+				
+		// Act
+		List<String> subGrids = verifier.getSubGrids("417369825632158947958724316825437169791586432346912758289643571573291684164875293");
+		result = subGrids.get(1);
+		
+		// Assert
+		assertEquals("369157624", result);
+	}
 }
