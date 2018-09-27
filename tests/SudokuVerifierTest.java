ffce3eba6fc9ecc04ccfd55aacb13a3cb8c0f9cd
Ismo Lundbom <ismo.lundbom@gmail.com>
2018-09-28 01:04:16 +0300

    Assignment finished

--- tests/SudokuVerifierTest.java
+++ tests/SudokuVerifierTest.java
@@ -78,12 +78,7 @@ public class SudokuVerifierTest {
 		assertEquals("Should be True", true, test);
 	}	
 	
-	@Test
-	public void testing(){
-		SudokuVerifier Sf = new SudokuVerifier();
-		Sf.verify("12345");
-	}	
-
+	
 	@Test
 	public void char_to_int(){
 		SudokuVerifier Sf = new SudokuVerifier();
@@ -104,7 +99,113 @@ public class SudokuVerifierTest {
 		int test = Sf.verify("417369825632158947958724316825437169791586432346912758289643571573291684164875293");
 		//System.out.println("test:"+test);
 		assertEquals("Should be 0 as it does nothing", 0, test);
-	}		
+	}
 	
+	@Test
+	public void rowChecker_test_1() {
+		SudokuVerifier Sf = new SudokuVerifier();
+		boolean test = Sf.rowChecker("123456789");
+		assertEquals("Should be true", true, test);
+		
+	}
+	
+	@Test
+	public void rowChecker_test_2() {
+		SudokuVerifier Sf = new SudokuVerifier();
+		boolean test = Sf.rowChecker("122456789");
+		assertEquals("Should be false", false, test);
+	}	
+	
+	@Test
+	public void rowdivider_test_1() {
+		SudokuVerifier Sf = new SudokuVerifier();
+		String test = Sf.rowDivider("417369825632158947958724316825437169791586432346912758289643571573291684164875293", 1);
+		assertEquals("Should be 417369825", "417369825", test);
+	}	
+	
+	@Test
+	public void rowdivider_test_2() {
+		SudokuVerifier Sf = new SudokuVerifier();
+		String test = Sf.rowDivider("417369825632158947958724316825437169791586432346912758289643571573291684164875293", 2);
+		assertEquals("Should be 632158947", "632158947", test);
+	}
+	
+	@Test
+	public void rowdivider_test_3() {
+		SudokuVerifier Sf = new SudokuVerifier();
+		String test = Sf.rowDivider("417369825622158947958724316825437169791586432346912758289643571573291684164875293", 2);
+		assertEquals("Should fail, inserted an error", "632158947", test);
+	}
+
+	@Test
+	public void groupdivider_test_1() {
+		SudokuVerifier Sf = new SudokuVerifier();
+		String test = Sf.groupDivider("417369825632158947958724316825437169791586432346912758289643571573291684164875293", 1);
+		assertEquals("Should be 417632958", "417632958", test);
+	}	
+	
+	@Test
+	public void columndivider_test_1() {
+		SudokuVerifier Sf = new SudokuVerifier();
+		String test = Sf.colDivider("417369825632158947958724316825437169791586432346912758289643571573291684164875293", 1);
+		assertEquals("Should be 469873251", "469873251", test);
+	}
+
+	@Test
+	public void intsOk_test_1() {
+		SudokuVerifier Sf = new SudokuVerifier();
+		Sf.verify("417369825632158947958724316825437169791586432346912758289643571573291684164875293");
+		boolean test = Sf.intsOk();
+		assertEquals("Should be true", true, test);
+	}
+	
+	@Test
+	public void intsOk_test_2() {
+		SudokuVerifier Sf = new SudokuVerifier();
+		int test = Sf.verify("417069825632158947958724316825437169791586432346912758289643571573291684164875293");
+		assertEquals("Should be true", -1, test);
+	}	
+	
+	@Test
+	public void verify_test_2() {
+		SudokuVerifier Sf = new SudokuVerifier();
+		int test = Sf.verify("417369825632158947958724316825437169791586432346912758289643571573291684164875293");
+		assertEquals("Should be true", 0, test);
+	}	
+
+	@Test
+	public void verify_test_3() {
+		SudokuVerifier Sf = new SudokuVerifier();
+		int test = Sf.verify("554469825632158947958724316825437169791586432346912758289643571573291684164875293");
+		assertEquals("Should be -3", -3, test);
+	}	
+	
+	@Test
+	public void verify_test_4() {
+		SudokuVerifier Sf = new SudokuVerifier();
+		int test = Sf.verify("554469825632158947958724316825437169791586432346912758289643571573291684164875293");
+		assertEquals("Should fail, as it should be -3", 0, test);
+	}	
+	
+	@Test
+	public void verify_test_5() {
+		SudokuVerifier Sf = new SudokuVerifier();
+		int test = Sf.verify("417369825432158947958724316825437169791586432346912758289643571573291684164875293");
+		assertEquals("Should be -4", -4, test);
+	}
+	
+	@Test
+	public void verify_test_6() {
+		SudokuVerifier Sf = new SudokuVerifier();
+		int test = Sf.verify("417369825602158947958724316825437169791586432346912758289643571573291684164875293");
+		assertEquals("Should be -1", -1, test);
+	}	
+
+	@Test
+	public void verify_test_7() {
+		SudokuVerifier Sf = new SudokuVerifier();
+		int test = Sf.verify("41736982563-2158947958724316825437169791586432346912758289643571573291684164875293");
+		assertEquals("Should be -1", -1, test);
+	}	
 }
 
