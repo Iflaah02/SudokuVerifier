da4cbabf6738967eb5892a61ba999858e79b6a84
LENOVO <LENOVO@DESKTOP-1T57PFP>
2018-09-30 16:15:33 +0530

    Assignment Finished

--- src/SudokuVerifier.java
+++ src/SudokuVerifier.java
@@ -1,10 +1,78 @@
 
 public class SudokuVerifier {
 	
-	public int verify(String candidateSolution) {
-		// returns 0 if the candidate solution is correct
-		return 0;
+	public int verify(String userSolution) {
+		//Check length is exactly 81 characters long or not
+		if (userSolution == null || userSolution.length() != 81)
+			return -1;
 		
-		//check zzz
+		
+		//R1: A cell in a Sudoku game can only store positive digits, i.e. 1...9.
+		//Return -1: means it is violating Rule #1
+		if (!userSolution.matches("[1-9]+"))
+			return -1;
+		
+		
+		//R2: All digits appear only once in a sub-grid, i.e. they cannot repeat.
+		//Return -2: means it is violating Rule #2
+		String[][] subGrids = new String[3][3];
+		for (int i = 0; i < 3; i++) {
+			for (int j = 0; j < 3; j++) {
+				subGrids[i][j] = "";
+			}
+		}
+		
+		int a, b;
+		for (int i = 0; i < userSolution.length(); i++) {
+			a = Math.floorDiv((i % 9), 3);
+			b = Math.floorDiv(Math.floorDiv(i, 9), 3);
+			
+			subGrids[a][b] = subGrids[a][b].concat(String.valueOf(userSolution.charAt(i)));
+		}
+		
+		for (int i = 0; i < 3; i++) {
+			for (int j = 0; j < 3; j++) {
+				for (int k = 1; k <= 9; k++)
+					subGrids[i][j] = subGrids[i][j].replaceFirst(String.valueOf(k), "");
+				
+				if (subGrids[i][j].length() != 0)
+					return -2;
+			}
+		}
+		
+		
+		//R3: A digit can appear only once in the rows of the global grid.
+		//Return -3: means it is violating Rule #3
+		String[] splitRows = new String[9];
+		for (int i = 0; i < 9; i++) {
+			splitRows[i] = userSolution.substring(i * 9, (i + 1) * 9);
+			
+			for (int j = 1; j <= 9; j++)
+				splitRows[i] = splitRows[i].replaceFirst(String.valueOf(j), "");
+
+			if (splitRows[i].length() != 0)
+				return -3;
+		}
+		
+		
+		//R4: A digit can appear only once in the columns of the global grid.
+		//Return -4: means it is violating Rule #4
+		String[] splitColumns = new String[9];
+		for (int i = 0; i < splitColumns.length; i++)
+			splitColumns[i] = "";
+		
+		for (int i = 0; i < 81; i++)
+			splitColumns[i % 9] = splitColumns[i % 9].concat(String.valueOf(userSolution.charAt(i)));
+		
+		for (int i = 0; i < splitColumns.length; i++) {
+			for (int j = 1; j <= 9; j++)
+				splitColumns[i] = splitColumns[i].replaceFirst(String.valueOf(j), "");
+			
+			if (splitColumns[i].length() != 0)
+				return -4;
+		}
+		
+		//Return 0: means it is a valid Sudoku solution
+		return 0;
 	}
 }
--- tests/SudokuVerifierTest.java
+++ tests/SudokuVerifierTest.java
@@ -4,12 +4,91 @@ import org.junit.Test;
 
 public class SudokuVerifierTest {
 
-// A correct Sudoku string: 417369825632158947958724316825437169791586432346912758289643571573291684164875293
-// An incorrect Sudoku string: 123456789912345678891234567789123456678912345567891234456789123345678912234567891
+// Given correct Sudoku string: 417369825632158947958724316825437169791586432346912758289643571573291684164875293
+// An incorrect Sudoku string: 111111122222222223333333334444444445555555555666666666777777777788888888899999999
 
+	
+	//Verification with correct Sudoku string
 	@Test
-	public void testVerify() {
-		fail("Not yet implemented");
+	public void testCorrect() {
+		SudokuVerifier verifier = new SudokuVerifier();
+		int returnCode = verifier.verify("417369825632158947958724316825437169791586432346912758289643571573291684164875293");
+		assertEquals(0, returnCode);
 	}
-
+	
+	//Verification with incorrect Sudoku string
+	@Test
+	public void testIncorrect() {
+		SudokuVerifier verifier = new SudokuVerifier();
+		int returnCode = verifier.verify("111111122222222223333333334444444445555555555666666666777777777788888888899999999");
+		assertEquals(-2, returnCode);
+	}
+	
+	//R1: A cell in a Sudoku game can only store positive digits, i.e. 1...9.
+	@Test
+	public void testIncorrect_R1() {
+		SudokuVerifier verifier = new SudokuVerifier();
+		int returnCode = verifier.verify(""
+				+ "417369825"
+				+ "632158947"
+				+ "958724316"
+				+ "825437169"
+				+ "79158asd2" //Non numbers added here
+				+ "346912758"
+				+ "289643571"
+				+ "573291684"
+				+ "164875293");
+		assertEquals(-1, returnCode); 
+	}
+	
+	//R2: All digits appear only once in a sub-grid, i.e. they cannot repeat.
+	@Test
+	public void testIncorrect_R2() {
+		SudokuVerifier verifier = new SudokuVerifier();
+		int returnCode = verifier.verify(""
+				+ "417369825"
+				+ "632158947"
+				+ "958724316"
+				+ "825437169"
+				+ "791586432"
+				+ "346912758"
+				+ "289643571"
+				+ "573291685" //Rule 2 violates here
+				+ "164875293");
+		assertEquals(-2, returnCode); 
+	}
+	
+	//R3: A digit can appear only once in the rows of the global grid.
+	@Test
+	public void testIncorrect_R3() {
+		SudokuVerifier verifier = new SudokuVerifier();
+		int returnCode = verifier.verify(""
+				+ "417369825"
+				+ "632158917" //Number 1 repeats in this raw
+				+ "958724346"
+				+ "825437169"
+				+ "791586432"
+				+ "346912758"
+				+ "289643571"
+				+ "573291684"
+				+ "164875293");
+		assertEquals(-3, returnCode); 
+	}
+	
+	//R4: A digit can appear only once in the columns of the global grid.
+	@Test
+	public void testIncorrect_R4() {
+		SudokuVerifier verifier = new SudokuVerifier();
+		int returnCode = verifier.verify(""
+				+ "417369825"
+				+ "632158947"
+				+ "958724316"
+				+ "825437169"
+				+ "791586432"
+				+ "346912758"
+				+ "289643571"
+				+ "537291684" //Number 3 repeats in 2nd column
+				+ "164875293");
+		assertEquals(-4, returnCode); //No repeating in a column
+	}
 }
