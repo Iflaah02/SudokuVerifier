1be67a2c8a22f3eec9170eb8a59e09a8e2379477
SilviaLi1225 <916621497@qq.com>
2018-09-30 15:30:49 +0300

    Assignment Finished

--- src/SudokuVerifier.java
+++ src/SudokuVerifier.java
@@ -1,10 +1,124 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+import java.util.TreeSet;
 
 public class SudokuVerifier {
 	
-	public int verify(String candidateSolution) {
-		// returns 0 if the candidate solution is correct
+	public int verify(String str){
+		if(verifyNumber(str) == -1){
+			return -1;
+		}
+		if(checkSubGrid(str) == -2){
+			return -2;
+		}
+		if(checkGlobalRow(str) == -3){
+			return -3;
+		}
+		if(checkGlobalColumn(str) == -4){
+			return -4;
+		}
 		return 0;
+	}
+
+	
+	public int verifyNumber(String candidateSolution){
+		char[] charSolution = candidateSolution.toCharArray();
+		int[] numbers = new int[charSolution.length];
 		
-		//check
+		if(charSolution.length != 81){
+			return -1;
+		}
+		
+		for(int i=0; i< charSolution.length; i++){
+			numbers[i] = Integer.parseInt(String.valueOf(candidateSolution.charAt(i)));
+			if(numbers[i] <=0 || numbers[i] > 9){
+				return -1;
+			}
+		}				
+		return 0;				
+	}
+	
+	
+	public int[][] convertToArray(String str){
+		int[] numbers = new int[str.length()];	
+		int[][] subGrid = new int[9][9];
+		
+		for(int m=0; m< str.length(); m++){
+			numbers[m] = Integer.parseInt(String.valueOf(str.charAt(m)));
+			}
+		
+		for(int i=0; i<9; i++){
+			for(int j=0; j<9; j++){
+				subGrid[i][j] = numbers[9*i+j];
+			}
+		}
+		
+		return subGrid;
+	}
+	
+	
+	public int checkGlobalRow(String str){
+		int[][] subGrid = convertToArray(str);
+		for(int i=0; i<subGrid.length; i++){
+			Set<Integer> set = new TreeSet<>();
+			for(int j=0; j<subGrid.length; j++){
+				set.add(subGrid[i][j]);
+			}			
+			if(set.size() != subGrid.length ){
+				return -3;
+			}
+		}
+		return 0;
+	}
+	
+	public int checkGlobalColumn(String str){
+		int[][] subGrid = convertToArray(str);
+		for(int i=0; i<subGrid.length; i++){
+			Set<Integer> set = new TreeSet<>();
+			for(int j=0; j<subGrid.length; j++){
+				set.add(subGrid[j][i]);
+			}			
+			if(set.size() != subGrid.length ){
+				return -4;
+			}
+		}
+		return 0;
+	}
+	
+	public int checkSubGrid(String str){
+		String[] str1 = new String[str.length()/3];
+		String[] str2 = new String[str.length()/9];
+		String temp = "";
+		int k=0;
+		int[] numbers = new int[str.length()/9];	
+
+		for(int i=0; i<27; i++){
+			str1[i] = str.substring(3*i, 3*i+3);
+		}
+		
+		while(k<9){		
+			for(int j=0; j<3; j++){
+				for(int i=0; i<3; i++){
+						temp = temp + str1[i+j*9]+str1[i+3+j*9]+str1[i+6+j*9];
+						str2[k] = temp;
+						k++;
+						temp = "";
+				}									
+			}		
+			}
+
+		for(int i=0; i<str2.length; i++){
+			Set<Integer> set = new TreeSet<>();
+			for(int m=0; m<str2[i].length(); m++){			
+				int a = Integer.parseInt(String.valueOf(str2[i].charAt(m)));
+				set.add(a);
+			}
+			if(set.size() != str2[i].length()){
+				return -2;
+			}
+		}		
+		
+		return 0;
 	}
 }
--- tests/SudokuVerifierParametrizedTest.java
+++ tests/SudokuVerifierParametrizedTest.java
@@ -5,8 +5,113 @@ import org.junit.Test;
 public class SudokuVerifierParametrizedTest {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testVerifyNumber_number() {
+		//Arrange
+		SudokuVerifier sudo = new SudokuVerifier();
+		//Act
+		int ans = sudo.verifyNumber("1234567891234567812345678");
+		assertEquals(-1,ans);
 	}
+	
+	@Test
+	public void testVerifyNumber_positive() {
+		//Arrange
+		SudokuVerifier sudo = new SudokuVerifier();
+		//Act
+		int ans = sudo.verifyNumber("-417369825632158947958724316825437169791586432346912758289643571573291684164875293");
+		assertEquals(-1,ans);
+	}
+	
+	@Test
+	public void testVerifyNumber_True() {
+		//Arrange
+		SudokuVerifier sudo = new SudokuVerifier();
+		//Act
+		int ans = sudo.verifyNumber("417369825632158947958724316825437169791586432346912758289643571573291684164875293");
+		assertEquals(0,ans);
+	}
+	
+	@Test
+	public void testConvertToArray() {
+		//Arrange
+		SudokuVerifier sudo = new SudokuVerifier();
+		//Act
+		int ans[][] = sudo.convertToArray("417369825632158947958724316825437169791586432346912758289643571573291684164875293");
+		assertEquals(9,ans.length);
+		assertEquals(9,ans[0].length);
+	}
+	
+	@Test
+	public void testCheckGlobalRow_1() {
+		//Arrange
+		SudokuVerifier sudo = new SudokuVerifier();
+		//Act
+		int ans = sudo.checkGlobalRow("417364825632158947958724316825437169791586432346912758289643571573291684164875293");
+		assertEquals(-3,ans);
+	}
+	
+	@Test
+	public void testCheckGlobalRow_9() {
+		//Arrange
+		SudokuVerifier sudo = new SudokuVerifier();
+		//Act
+		int ans = sudo.checkGlobalRow("417369825632158947958724316825437169791586432346912758289643571573291684164885293");
+		assertEquals(-3,ans);
+	}
+	
+	@Test
+	public void testCheckGlobalRow_True() {
+		//Arrange
+		SudokuVerifier sudo = new SudokuVerifier();
+		//Act
+		int ans = sudo.checkGlobalRow("417369825632158947958724316825437169791586432346912758289643571573291684164875293");
+		assertEquals(0,ans);
+	}
+	
+	@Test
+	public void testCheckGlobalColumn_1() {
+		//Arrange
+		SudokuVerifier sudo = new SudokuVerifier();
+		//Act
+		int ans = sudo.checkGlobalColumn("417369825432158947958724316825437169791586432346912758289643571573291684164875293");
+		assertEquals(-4,ans);
+	}
+	
+	@Test
+	public void testCheckGlobalColumn_True() {
+		//Arrange
+		SudokuVerifier sudo = new SudokuVerifier();
+		//Act
+		int ans = sudo.checkGlobalColumn("417369825632158947958724316825437169791586432346912758289643571573291684164875293");
+		assertEquals(0,ans);
+	}
+	
+	@Test
+	public void testCheckSubGrid_1() {
+		//Arrange
+		SudokuVerifier sudo = new SudokuVerifier();
+		//Act
+		int ans = sudo.checkSubGrid("417369825442158947958724316825437169791586432346912758289643571573291684164875293");
+		assertEquals(-2,ans);
+	}
+	
+	@Test
+	public void testCheckSubGrid_9() {
+		//Arrange
+		SudokuVerifier sudo = new SudokuVerifier();
+		//Act
+		int ans = sudo.checkSubGrid("417369825632158947958724316825437169791586432346912758289643571573291634164875293");
+		assertEquals(-2,ans);
+	}
+	
+	@Test
+	public void testCheckSubGrid_True() {
+		//Arrange
+		SudokuVerifier sudo = new SudokuVerifier();
+		//Act
+		int ans = sudo.checkSubGrid("417369825632158947958724316825437169791586432346912758289643571573291684164875293");
+		assertEquals(0,ans);
+	}
+	
 
 }
