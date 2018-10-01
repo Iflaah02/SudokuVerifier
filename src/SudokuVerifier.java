259ae2e7c8546e9ed8d536a87bdc6979fb911709
attet <attetihinen@gmail.com>
2018-10-01 08:17:46 +0300

    Assignment finished.

--- src/SudokuVerifier.java
+++ src/SudokuVerifier.java
@@ -1,10 +1,163 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+
 
 public class SudokuVerifier {
 	
+	private final int side = 9;
+	
 	public int verify(String candidateSolution) {
-		// returns 0 if the candidate solution is correct
-		return 0;
 		
-		//check
+		if (candidateSolution.length() != side*side) {
+			return -1; 
+		}
+		
+		int index = 0;
+		int row = 0;
+		int column = 0;
+	
+		final List<List<Integer>> grid = new ArrayList<List<Integer>>();
+				
+		while (index < candidateSolution.length()) {
+			
+			char character = candidateSolution.charAt(index);
+			row = index / side;
+			column = index % side;
+			
+			if (column == 0) {
+				grid.add(new ArrayList<Integer>());
+			}
+										
+			if (Character.isDigit(character) == false) {
+				return -1;
+			} else {
+				grid.get(row).add(Integer.parseInt(String.valueOf(character)));
+			}
+			
+			index++;
+		}
+		
+		if (checkDigitAppearOnlyOnceInSubGrids(grid) == -2) {
+			return -2;
+		}
+		
+		if (checkDigitAppearOnlyOnceInRows(grid) == -3) {
+			return -3;
+		}
+		
+		if (checkDigitAppearOnlyOnceInColumns(grid) == -4) {
+			return -4;
+		}
+							
+		return 0;
+	}
+
+	private int checkDigitAppearOnlyOnceInSubGrids(List<List<Integer>> grid) {
+		
+		List<List<Integer>> subGrids = new ArrayList<List<Integer>>();
+		
+		for (int i = 0; i < side; i++) {
+			subGrids.add(new ArrayList<Integer>());
+		}
+		
+		for (int row = 0; row < side; ++row) {
+			for (int column = 0; column < side; ++column) {
+				if (column < 3 && row < 3) {
+					subGrids.get(0).add(grid.get(row).get(column));
+				}
+				
+				if (column > 2 && column < 6 && row < 3) {
+					subGrids.get(1).add(grid.get(row).get(column));
+				}
+				
+				if (column > 5 && column < side && row < 3) {
+					subGrids.get(2).add(grid.get(row).get(column));
+				}
+				
+				if (column < 3 && row > 2 && row < 6) {
+					subGrids.get(3).add(grid.get(row).get(column));
+				}
+				
+				if (column > 2 && column < 6 && row > 2 && row < 6) {
+					subGrids.get(4).add(grid.get(row).get(column));
+				}
+				
+				if (column > 5 && column < side && row > 2 && row < 6) {
+					subGrids.get(5).add(grid.get(row).get(column));
+				}
+				
+				if (column < 3 && row > 5 && row < side) {
+					subGrids.get(6).add(grid.get(row).get(column));
+				}
+				
+				if (column > 2 && column < 6 && row > 5 && row < side) {
+					subGrids.get(7).add(grid.get(row).get(column));
+				}
+				
+				if (column > 5 && column < 9 && row > 5 && row < 9) {
+					subGrids.get(8).add(grid.get(row).get(column));
+				}
+			}
+		}
+		
+		for (int i = 0; i < side; i++) {
+			
+			Collections.sort(subGrids.get(i));
+			
+			for (int j = 0; j < side; j++) {
+				if (j < 8 && subGrids.get(i).get(j) 
+						== subGrids.get(i).get(j + 1) ) {
+					return -2;
+				}
+			}
+		}
+		
+		return 0;
+	}
+ 
+	private int checkDigitAppearOnlyOnceInRows(List<List<Integer>> grid) {
+		for (int row = 0; row < side; row++) {
+			
+			List<Integer> gridRow = new ArrayList<Integer>();
+			
+			for (int column = 0; column < side; column++) {
+				gridRow.add(grid.get(row).get(column));
+			}
+			
+			Collections.sort(gridRow);
+			
+			for (int column = 0; column < side; column++) {
+			
+				if (column < 8 && gridRow.get(column) 
+						== gridRow.get(column + 1)) {
+					return -3;
+				}
+			}
+		}
+		
+		return 0;
+	}
+	
+	private int checkDigitAppearOnlyOnceInColumns(List<List<Integer>> grid) {
+		for (int column = 0; column < side; column++) {
+			
+			List<Integer> gridColumn = new ArrayList<Integer>();
+			
+			for (int row = 0; row < side; row++) {
+				gridColumn.add(grid.get(row).get(column));
+			}
+			
+			Collections.sort(gridColumn);
+			
+			for (int row = 0; row < side; row++) {
+				if (row < 8 && gridColumn.get(row) == gridColumn.get(row + 1)) {
+					return -4;
+				}
+			}
+		}
+		
+		return 0;
 	}
 }
--- tests/SudokuVerifierTest.java
+++ tests/SudokuVerifierTest.java
@@ -4,12 +4,114 @@ import org.junit.Test;
 
 public class SudokuVerifierTest {
 
-// A correct Sudoku string: 417369825632158947958724316825437169791586432346912758289643571573291684164875293
-// An incorrect Sudoku string: 123456789912345678891234567789123456678912345567891234456789123345678912234567891
-
 	@Test
-	public void testVerify() {
-		fail("Not yet implemented");
+	public void testWithViolationgRule1() {
+		SudokuVerifier verifier = new SudokuVerifier();
+		//Letter C is in second row
+		String string = "417369825"
+				      + "632158C47"
+				      + "958724316"
+				      + "825437169"
+				      + "791586432"
+				      + "346912758"
+				      + "289643571"
+				      + "573291684"
+				      + "164875293";
+		assertEquals(-1, verifier.verify(string));
 	}
-
+	
+	@Test
+	public void testWithViolationgRule1TooShortString() {
+		SudokuVerifier verifier = new SudokuVerifier();
+		//One row is missing
+		String string = "417369825"
+				      + "632158C47"
+				      + "958724316"
+				      + "825437169"
+				      + "791586432"
+				      + "346912758"
+				      + "289643571"
+				      + "573291684";
+		assertEquals(-1, verifier.verify(string));
+	}
+	
+	@Test
+	public void testWithViolationgRule1TooLongString() {
+		SudokuVerifier verifier = new SudokuVerifier();
+		//One row too much
+		String string = "417369825"
+				      + "632158C47"
+				      + "958724316"
+				      + "825437169"
+				      + "791586432"
+				      + "346912758"
+				      + "289643571"
+				      + "289643571"
+				      + "573291684";
+		assertEquals(-1, verifier.verify(string));
+	}
+	
+	@Test
+	public void testWithViolationRule2() {
+		SudokuVerifier verifier = new SudokuVerifier();
+		//Number 1 is twice in first sub grid
+		String string = "417369825"
+					  + "631258947"
+					  + "958724316"
+					  + "825437169"
+					  + "791586432"
+					  + "346912758"
+					  + "289643571"
+					  + "573291684"
+					  + "164875293";
+		assertEquals(-2, verifier.verify(string));
+	}
+	
+	@Test
+	public void testWithViolationRule3() {
+		SudokuVerifier verifier = new SudokuVerifier();
+		//Number 8 is twice in first row
+		String string = "418369825"
+					  + "632158947"
+					  + "957724316"
+					  + "825437169"
+					  + "791586432"
+					  + "346912758"
+					  + "289643571"
+					  + "573291684"
+					  + "164875293";
+		assertEquals(-3, verifier.verify(string));
+	}
+	
+	@Test
+	public void testWithViolationRule4() {
+		SudokuVerifier verifier = new SudokuVerifier();
+		//Number 4 is twice in first column
+		String string = "417369825"
+					  + "632158947"
+					  + "958724316"
+					  + "825437169"
+					  + "791586432"
+					  + "346912758"
+					  + "289643571"
+					  + "573291684"
+					  + "461875293";
+		assertEquals(-4, verifier.verify(string));
+	}
+		
+	@Test
+	public void testWithValid() {
+		SudokuVerifier verifier = new SudokuVerifier();
+		//Valid sudoku grid
+		String string = "417369825"
+					  + "632158947"
+					  + "958724316"
+					  + "825437169"
+					  + "791586432"
+					  + "346912758"
+					  + "289643571"
+					  + "573291684"
+					  + "164875293";
+		assertEquals(0, verifier.verify(string));
+	}
 }
