c6e0f79f418000bf409b1ba79b100f9762f4f357
Pepetti <pirkkapekka@gmail.com>
2018-10-01 21:59:37 +0300

    Assignment Finished

--- tests/SudokuVerifierTest.java
+++ tests/SudokuVerifierTest.java
@@ -6,10 +6,38 @@ public class SudokuVerifierTest {
 
 // A correct Sudoku string: 417369825632158947958724316825437169791586432346912758289643571573291684164875293
 // An incorrect Sudoku string: 123456789912345678891234567789123456678912345567891234456789123345678912234567891
-
+	
 	@Test
-	public void testVerify() {
-		fail("Not yet implemented");
+	public void testVerifierCorrect() {
+		//Arrange
+		SudokuVerifier verifier = new SudokuVerifier();
+		String candidateSolution = "417369825632158947958724316825437169791586432346912758289643571573291684164875293";
+		//Act
+		int result = verifier.verify(candidateSolution);
+		//Assertion
+		assertEquals(0, result);
+		System.out.println(result);
+	}
+	@Test
+	public void testVerifierIncorrect() {
+		//Arrange
+		SudokuVerifier verifier = new SudokuVerifier();
+		String candidateSolution = "123456789912345678891234567789123456678912345567891234456789123345678912234567891"; 	
+		//Act
+		int result = verifier.verify(candidateSolution);
+		//Assertion
+		assertEquals(-2, result);
+	}
+	
+	@Test
+	public void testVerifierNumberIncorrect() {
+		//Arrange
+		SudokuVerifier verifier = new SudokuVerifier();
+		String candidateSolution = "123456789912345678891234567789123456678912345567891234456789123345678912234567890"; 	
+		//Act
+		int result = verifier.verify(candidateSolution);
+		//Assertion
+		assertEquals(-1, result);
 	}
 
 }
