f8889db94053794e1b7c3153547f25510913cc6c
Sonofhades <jukka.uhlgren@merzenarie.com>
2018-10-01 23:55:09 +0300

    Assignment 2 done

--- src/SudokuVerifier.java
+++ src/SudokuVerifier.java
@@ -1,10 +1,124 @@
+import com.sun.xml.internal.ws.util.StringUtils;
+
 
 public class SudokuVerifier {
 	
 	public int verify(String candidateSolution) {
+		int result;
+		result = checkLength(candidateSolution);
+		result = checkDigits(candidateSolution);
+		result = checkRows(candidateSolution);
+		result = checkColumns(candidateSolution);
 		// returns 0 if the candidate solution is correct
-		return 0;
+		return result;
 		
 		//check
 	}
+	
+	
+	public int checkLength(String x)
+		{
+		if (x.length() != 81) 
+			{
+			// System.out.println("Feed has incorrect number of digits");
+			return 1;
+			}
+		else return 0;
+		}
+	
+	public int checkDigits(String x)
+		{
+		int result=0;
+		// char c;
+		for (int i=0; i<x.length(); i++)
+			{
+			if (Character.isDigit(x.charAt(i)) != true || x.charAt(i) == '0')
+				{
+				result = -1;
+				}
+									
+			}
+		return result;
+		}
+	
+	public int checkNine(String x)
+		{
+		int result = 0; 
+		int i, j;
+		char c;
+		int counter=0;
+		for (i=0; i<9; i++)
+			{
+			c = x.charAt(i);
+			for (j=0; j<9; j++)
+				{
+				if (c == x.charAt(j)) counter++;
+				}
+			if (counter > 1) result = 1;
+			counter=0;
+			}
+		return result;
+		}
+	
+	public int checkRows(String x)
+		{
+		int result = 0;
+		int i, j;
+		int start;
+		int end;
+		String [] row = new String[10];
+		for (i=0; i<9; i++) row[i]="";
+		for (i=0; i<9; i++)
+			{
+			for (j=0; j<9; j++)
+				{
+				row[i]=row[i]+x.charAt(i*9+j);
+				}
+			
+			result=checkNine(row[i]);
+			// System.out.println(row[i] + " " + result);
+			}
+		if (result == 1) result =-3;
+		return result;
+		
+		}
+	
+	public int checkColumns(String x)
+	
+	{
+	int result = 0;
+	int i,j;
+	int character_wanted=0; 
+	
+	String [] column = new String[10];
+	for (i=0; i<10; i++) column[i]="";
+	for (i=0; i<9; i++)
+		{
+		for (j=0; j<9; j++)
+			{
+			character_wanted = i+j*9;
+			// System.out.println("cw = " + character_wanted + " number " + x.charAt(character_wanted));
+			column[i]=column[i] + x.charAt(character_wanted);
+			}
+			result=checkNine(column[i]);
+			// System.out.println(column[i] + " " + result);
+		}
+	if (result == 1) result =-4;
+	return result;
+	
+	}
+	
+	// completely useless after finding out about split - command for strings
+		public String separate (String a, int x, int y) //
+		{
+		return a.substring(x, y);	
+		}	
+	// an attempt to solve the problem by counting 1-9 together on each row / column. 
+	// gets you into problem with any checks that have multiples with 45 in total.
+		public int toInt (char c)
+		{
+		int result = Character.getNumericValue(c); 
+		return result;	
+		}
+	
 }
--- tests/SudokuVerifierTest.java
+++ tests/SudokuVerifierTest.java
@@ -1,5 +1,6 @@
 import static org.junit.Assert.*;
 
+import org.junit.Ignore;
 import org.junit.Test;
 
 public class SudokuVerifierTest {
@@ -9,7 +10,94 @@ public class SudokuVerifierTest {
 
 	@Test
 	public void testVerify() {
-		fail("Not yet implemented");
+		SudokuVerifier s = new SudokuVerifier();
+		String str="123456789912345678891234567789123456678912345567891234456789123345678912234567891";
+		int result = s.verify(str);
+		if (result == -1) fail("Sudoku had invalid characters");
 	}
+	
+	@Test
+	public void testcheckLength_with_valid_length() 
+		{
+		SudokuVerifier s = new SudokuVerifier();
+		String str="123456789912345678891234567789123456678912345567891234456789123345678912234567891";
+		int result = s.checkLength(str);
+		if (result != 0) fail("Size fails");
+		}
+	
+	@Test
+	public void testcheckLength_with_invalid_length() 
+		{
+		SudokuVerifier s = new SudokuVerifier();
+		String str="12345678991234567812345677823456678912345567891234456789123345678912234567891";
+		int result = s.checkLength(str);
+		if (result != 0) fail("Size fails");
+		}
+	
+	
+	@Test
+	public void testcheckDigits_with_valid_input() 
+		{
+		SudokuVerifier s = new SudokuVerifier();
+		String str="123456789912345678891234567789123456678912345567891234456789123345678912234567891";
+		int result = s.checkDigits(str);
+		if (result != 0) fail("Invalid characters in feed");
+		}
+	
+	@Test
+	public void testcheckDigits_with_invalid_input() // one zero inserted to feed
+		{
+		SudokuVerifier s = new SudokuVerifier();
+		String str="123456789912345678891230567789123456678912345567891234456789123345678912234567891";
+		int result = s.checkDigits(str);
+		if (result != 0) fail("Invalid characters in feed");
+		}
+	
+	@Test
+	public void testMethod_checkNine_with_invalid_input() // double 3s
+		{
+		SudokuVerifier s = new SudokuVerifier();
+		String str="123345678";
+		int result = s.checkNine(str);
+		if (result != 0) fail("One or more numbers appear twice");
+		}
+	
+	@Test
+	public void testMethod_checkNine_with_valid_input() 
+		{
+		SudokuVerifier s = new SudokuVerifier();
+		String str="123456789";
+		int result = s.checkNine(str);
+		if (result != 0) fail("One or more numbers appear twice");
+		}
+	
+	
+	@Ignore
+	public void test_checkRows_with_valid_input() 
+		{
+		SudokuVerifier s = new SudokuVerifier();
+		String str="417369825632158947958724316825437169791586432346912758289643571573291684164875293";
+		int result = s.checkRows(str);
+		if (result == -3) fail("One or more rows had more than one of the same number");
+		}
+	
+	@Test
+	public void test_checkRows_with_invalid_input() // inserted double 3s on the 1st row
+		{
+		SudokuVerifier s = new SudokuVerifier();
+		String str="417339825632158947958724316825437169791586432346912758289643571573291684164875293";
+		int result = s.checkRows(str);
+		if (result == -3) fail("One or more rows had more than one of the same number");
+		}
+	
+	@Test
+	public void test_checkColumns_with_valid_input() 
+		{
+		SudokuVerifier s = new SudokuVerifier();
+		String str="417369825632158947958724316825437169791586432346912758289643571573291684164875293";
+		int result = s.checkColumns(str);
+		if (result == -4) fail("One or more rows had more than one of the same number");
+		}
+	
 
 }
