bd1523999a6d8f71ca83acb4a5463ccd85155b0b
Tapio Anttila <tapio-a@hotmail.com>
2018-10-05 21:05:19 +0300

    Assignment Finished

--- src/SudokuVerifier.java
+++ src/SudokuVerifier.java
@@ -4,7 +4,7 @@ import java.util.List;
 public class SudokuVerifier {
 	
 	public int verify(String candidateSolution) {
-		int r = 1;
+		int r = -1;
 		
 		if (checkInput(candidateSolution)) {
 			if (!checkRuleOne(candidateSolution)) r = -1;
@@ -16,8 +16,7 @@ public class SudokuVerifier {
 		
 		// returns 0 if the candidate solution is correct
 		return r;
-		
-		//check
+
 	}
 	
 	public Boolean checkInput(String candidateSolution) {
@@ -38,35 +37,64 @@ public class SudokuVerifier {
 	
 	public Boolean checkRuleTwo(String candidateSolution) {
 		//"All digits appear only once in a sub-grid, i.e. they cannot repeat"
+		char c; String sg = "";
+		String[] rows = getRows(candidateSolution);
+		List<String> sgList = new ArrayList<>();
+
+		for (int r = 0; r < 9; r++) {
+			for (int i = 0; i < 3; i+=3) {
+				sgList.add(rows[r].substring(i, Math.min(9, i + 3)));
+			}	
+		}
+	
+		for (int r = 0; r < 9; r++) {
+			for (int i = 3; i < 6; i+=3) {
+				sgList.add(rows[r].substring(i, Math.min(9, i + 3)));
+			}
+		}
 		
+		for (int r = 0; r < 9; r++) {
+			for (int i = 6; i < 9; i+=3) {
+				sgList.add(rows[r].substring(i, Math.min(9, i + 3)));
+			}
+		}
+		
+		List<String> cleanList = new ArrayList<>();
+		for (int j = 0; j < 27; j+=3) {
+			String temp = sgList.get(j) + sgList.get(j+1) + sgList.get(j+2);
+			temp.replaceAll(", ", "");
+			cleanList.add(temp);
+		}
+		for (int i = 0; i < 9; i++) {
+			String x = cleanList.get(i);
+			if (!uniqueCharacters(x)) return false;
+		}
+
 		return true;
 	}
 	
-	public String[] extractRows(String candidateSolution){
-		List<String> rowList = new ArrayList<>();
+		public String[] getRows(String candidateSolution){
+			List<String> rowList = new ArrayList<>();
+			
+			for (int i = 0; i < 81; i += 9) {
+	            rowList.add(candidateSolution.substring(i, Math.min(81, i + 9)));
+	        }
+			
+			return rowList.toArray(new String[0]);
+		}
 		
-		for (int i = 0; i < 81; i += 9) {
-            rowList.add(candidateSolution.substring(i, Math.min(81, i + 9)));
-        }
-		
-		return rowList.toArray(new String[0]);
-	}
-	public boolean uniqueCharacters(String str) 
-    { 
-        // If at any time we encounter 2 same 
-        // characters, return false 
-        for (int i = 0; i < str.length(); i++) 
-            for (int j = i + 1; j < str.length(); j++) 
-                if (str.charAt(i) == str.charAt(j)) 
-                    return false; 
-  
-        // If no duplicate characters encountered, 
-        // return true 
-        return true; 
-    } 
+		public boolean uniqueCharacters(String str) 
+	    {
+	        for (int i = 0; i < str.length(); i++) 
+	            for (int j = i + 1; j < str.length(); j++) 
+	                if (str.charAt(i) == str.charAt(j)) 
+	                    return false; 
+	  
+	        return true; 
+	    } 
 	public Boolean checkRuleThree(String candidateSolution) {
 		//"A digit can appear only once in the rows of the global grid."
-		String[] rows = extractRows(candidateSolution);
+		String[] rows = getRows(candidateSolution);
 		
 		for (int i = 0; i < 9; i++) {
 			if (!uniqueCharacters(rows[i])) return false;
@@ -74,8 +102,29 @@ public class SudokuVerifier {
 		return true;
 	}
 	
+		public String[] getCols(String candidateSolution) {
+			String[] rows = getRows(candidateSolution);
+			List<String> colList = new ArrayList<>();
+			String col = "";
+			char c;
+			
+			for(int r = 0; r < 9; r++) {
+				for (int i = 0; i < 9; i++) {
+					c = rows[i].charAt(r);
+					col += c;
+				}
+				colList.add(col);
+				col = "";
+			}
+			return colList.toArray(new String[0]);
+		}
 	public Boolean checkRuleFour(String candidateSolution) {
 		//"A digit can appear only once in the columns of the global grid."
+		String[] cols = getCols(candidateSolution);
+		
+		for (int i = 0; i < 9; i++) {
+			if (!uniqueCharacters(cols[i])) return false;
+		}
 		
 		return true;
 	}
